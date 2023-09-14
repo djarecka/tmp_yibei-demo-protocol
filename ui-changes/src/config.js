8f296f54a5d98c3df39fdea081f6df6f06ab75eb
Dorota Jarecka <djarecka@gmail.com>
2023-09-14 17:16:35 -0400

    initial

---
+++ ui-changes/src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  /* eslint-disable */
+  githubSrc: 'https://github.com/yibeichan/demo-protocol/blob/master/DemoProtocol/DemoProtocol_schema',
+  banner: 'This is a demonstration protocol for ReproSchema.',
+  startButton: 'Join',
+  assetsPublicPath: '/demo-protocol/',
+  backendServer: null
+};
