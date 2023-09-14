93b8ae3eaf988f1acc44576555c221153ef3dd4d
Dorota Jarecka <djarecka@gmail.com>
2023-09-14 17:18:21 -0400

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://github.com/yibeichan/demo-protocol/blob/master/DemoProtocol/DemoProtocol_schema',
+  githubSrc: 'https://github.com/djarecka/tmp_yibei-demo-protocol/blob/master/DemoProtocol/DemoProtocol_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
